abf404c73f02a267eec1f354b7e4f374ead784b0
notoriginallink <toldan04@yandex.ru>
2024-08-02 21:06:52 +0300

    feat: add main class

---
+++ src/main/java/ru/tolstov/translator/TranslatorApp.java
@@ -0,0 +1,11 @@
+package ru.tolstov.translator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TranslatorApp {
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(TranslatorApp.class, args);
+    }
+}
