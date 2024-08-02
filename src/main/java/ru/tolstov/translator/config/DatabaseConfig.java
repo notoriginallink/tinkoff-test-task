dcc28997e25646df09554eac5a44071fe72ff1e8
notoriginallink <toldan04@yandex.ru>
2024-08-02 21:05:52 +0300

    refactor: now DatabaseConfig gets connection properties from application.properties file

--- src/main/java/ru/tolstov/translator/config/DatabaseConfig.java
+++ src/main/java/ru/tolstov/translator/config/DatabaseConfig.java
@@ -1,6 +1,7 @@
 package ru.tolstov.translator.config;
 
 import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 
 import java.sql.Connection;
@@ -8,14 +9,19 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.Properties;
 
-// TODO url in separate config file
 @Configuration
 public class DatabaseConfig {
+    @Value("${spring.datasource.url}")
+    private static String url;
+    @Value("${spring.datasource.username}")
+    private static String username;
+    @Value("${spring.datasource.password}")
+    private static String password;
+
     public static Connection getConnection() throws SQLException {
-        final String url = "jdbc:postgresql://localhost:5432/translator";
         Properties props = new Properties();
-        props.setProperty("user", "daniel");
-        props.setProperty("password", "secret");
+        props.setProperty("user", username);
+        props.setProperty("password", password);
 
         return DriverManager.getConnection(url, props);
     }
